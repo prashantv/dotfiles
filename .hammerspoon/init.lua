69f5e67443fbbae99f4823b5b97269c95d440334
Prashant Varanasi <prashant@uber.com>
2016-03-22 11:23:05 -0700

    Add keyboard shortcuts to move window between screens

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -166,6 +166,28 @@ end)
 
 
 
+function bindMoveToScreen(key, f)
+  hs.hotkey.bind({"cmd", "ctrl", "shift"}, key, function()
+    local win = hs.window.focusedWindow()
+    f(win)
+  end)
+end
+
+
+-- Move window to screen on left/right/top/bottom.
+bindMoveToScreen("left", function(w)
+  w:moveOneScreenWest()
+end)
+bindMoveToScreen("right", function(w)
+  w:moveOneScreenEast()
+end)
+bindMoveToScreen("up", function(w)
+  w:moveOneScreenNorth()
+end)
+bindMoveToScreen("down", function(w)
+  w:moveOneScreenSouth()
+end)
+
 
 hs.hints.style = "vimperator"
 
