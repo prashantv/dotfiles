781381e2342bd625b24e8dd6880ecb9390c782a9
Prashant Varanasi <prashant@uber.com>
2016-03-22 11:23:14 -0700

    Add all keyboard shortcuts to top of the init.lua

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,4 +1,15 @@
 
+-- Shortcuts added by this script:
+-- Ctrl+Cmd+[NUM]: Switch to app (prefer cur screen).
+--  1 = Chrome, 2 = iTerm, 3 = Atom, 4 = HipChat
+-- Alt+Tab: Show window switcher list popup
+-- Ctrl+Cmd+[Arrow]: Take up some half of the screen. Up maximizes, then takes up top half.
+-- Ctrl+Cmd+[hjkl]: Resize window (make smaller or larger in given direction)
+-- Ctrl+Cmd+Shift+[hjkl]: Move window in the given direction
+-- Ctrl+Cmd+Shift+Tab: Common actions (e.g. lock screen)
+-- Ctrl+Cmd+Shift+[Arrow]: Move window to screen in given direction.
+
+
 -- Custom notifications for the screen
 -- hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
 --   hs.alert.show("Hello World!")
