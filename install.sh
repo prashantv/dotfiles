0ea17406eace47f55fdd4d5357bd4e887cf47d65
Prashant V <prashant@prashantv.com>
2021-09-12 10:25:48 -0700

    Shallow clone zprezto + submodules

--- install.sh
+++ install.sh
@@ -22,11 +22,11 @@ ln -sf `pwd`/.inputrc ~/
 touch ~/.zshrc_local
 [ -d ~/.zprezto ] &&
   (cd ~/.zprezto; git pull; git submodule update --init --recursive) \
-  || git clone --recursive https://github.com/sorin-ionescu/prezto.git ~/.zprezto
+  || git clone --depth 1 --recursive --shallow-submodules https://github.com/sorin-ionescu/prezto.git ~/.zprezto
 # zsh plugin
 [ -d ~/.zprezto-contrib/zsh-z ] &&
   (cd ~/.zprezto-contrib/zsh-z; git pull) \
-  || git clone https://github.com/agkozak/zsh-z.git ~/.zprezto-contrib/zsh-z
+  || git clone --depth 1 https://github.com/agkozak/zsh-z.git ~/.zprezto-contrib/zsh-z
 
 # git
 ln -sf `pwd`/.gitconfig ~/
