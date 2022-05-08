0f65a5f848b73567e55085185d45f11f0b206ac4
Prashant V <prashant@prashantv.com>
2022-05-08 10:03:22 -0700

    Install fzf, zsh integration and fzf-tmux

--- bin.sh
+++ bin.sh
@@ -6,18 +6,6 @@ IFS=$'\n\t'
 
 # Download the latest releases and install it to ~/bin
 
-echo "== FZF =="
-mkdir -p .extracted/fzf
-./dl-github.sh junegunn/fzf | tar xvz -C .extracted/fzf && install -m 755 .extracted/fzf/fzf ~/bin/
-
-echo "  binary from release"
-./dl-github.sh junegunn/fzf | tar xvz -C .extracted/fzf && install -m 755 .extracted/fzf/fzf ~/bin/
-echo "  fzf-tmux binary"
-./dl-raw-github.sh junegunn/fzf bin/fzf-tmux > .extracted/fzf/fzf-tmux && install -m 755 .extracted/fzf/fzf-tmux ~/bin/
-echo "  zsh completions"
-./dl-raw-github.sh junegunn/fzf shell/completion.zsh > .extracted/fzf/completion.zsh
-./dl-raw-github.sh junegunn/fzf shell/key-bindings.zsh > .extracted/fzf/key-bindings.zsh
-
 echo "== tmux-fastcopy =="
 mkdir -p .extracted/tmux-fastcopy
 echo "  binary from release"
