df0205935cdb51a05c29dc48c7147e9e603f8291
Prashant V <prashant@prashantv.com>
2022-05-08 10:16:38 -0700

    Install tmux-fastcopy from latest release

--- bin.sh
+++ bin.sh
@@ -6,8 +6,3 @@ IFS=$'\n\t'
 
 # Download the latest releases and install it to ~/bin
 
-echo "== tmux-fastcopy =="
-mkdir -p .extracted/tmux-fastcopy
-echo "  binary from release"
-./dl-github.sh abhinav/tmux-fastcopy | tar xvz -C .extracted/tmux-fastcopy && install -m 755 .extracted/tmux-fastcopy/tmux-fastcopy ~/bin/
-
