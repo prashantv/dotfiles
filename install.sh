a477b7d6970389d3cf6ce9caf8c085841d55936b
Prashant V <prashant@prashantv.com>
2022-05-07 22:54:34 -0700

    Create ~/tmp using a script
    create_ attribute only work for files, not directories,
    so use a script to create the ~/tmp directory instead.

--- install.sh
+++ install.sh
@@ -9,8 +9,5 @@ cd "$(dirname "$0")"
 
 touch ~/.zshrc_local
 
-# create .tmp
-[ -d ~/tmp ] || mkdir ~/tmp
-
 # disable homebrew analytics always
 command -v brew >/dev/null 2>&1 && brew analytics off
